7ce13ffecfe74e7323bbf6c8fff2ec9b48606f88
Rafael Kohler <Rafael Kohler@DESKTOP-3L24D37>
2019-03-28 23:04:44 -0300

    commit

---
+++ exerciciosJava/src/lista04/Livro.java
@@ -0,0 +1,76 @@
+package lista04;
+
+import java.util.ArrayList;
+
+/**
+ * Classe para representar um Livro
+ * @author 1810647
+ *
+ */
+public class Livro {
+
+	private String codigo;
+	
+	private String titulo;
+	
+	private ArrayList<String> autores;
+	
+	private String isbn;
+	
+	private Integer ano;
+	
+	public Livro(String codigo, String titulo, ArrayList<String> autores, String isbn, Integer ano) {
+		this.codigo = codigo;
+		this.titulo = titulo;
+		this.autores = autores;
+		this.isbn = isbn;
+		this.ano = ano;
+	}
+	
+	public Livro(){
+		
+	}
+	
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public ArrayList<String> getAutores() {
+		return autores;
+	}
+	public void setAutores(ArrayList<String> autores) {
+		this.autores = autores;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public Integer getAno() {
+		return ano;
+	}
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+	
+	public void adicionarAutroes(String autor) {
+		getAutores().add(autor);
+	}
+	
+	@Override
+	public String toString() {
+		return "\nCódigo: " + getCodigo() + "\nTítulo: " + getTitulo() + "\nAutor: " + getAutores() + "\nISBN: "
+				+ getIsbn() + "\nAno: " + getAno();
+	}
+	
+}
---
+++ exerciciosJava/src/lista04/Principal.java
@@ -0,0 +1,51 @@
+package lista04;
+
+import java.util.ArrayList;
+
+/**
+ * Classe para executar os exercícios da lista 004.
+ * @author 1810647
+ *
+ */
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Livro livro01 = new Livro();
+		livro01.setCodigo("1598FHK");
+		livro01.setTitulo("Core Java 2");
+		
+		ArrayList<String> autoresLivro01 = new ArrayList<>();
+		autoresLivro01.add("Cay S. Horstmann");
+		autoresLivro01.add("Gary Cornell");
+		
+		livro01.setAutores(autoresLivro01);
+		livro01.setIsbn("0130819336");
+		livro01.setAno(1999);
+		System.out.println("Livro 01:" + livro01);
+		
+		Livro livro02 = new Livro();
+		livro02.setCodigo("9865PLO");
+		livro02.setTitulo("Java, How to Program");
+		
+		ArrayList<String> autoresLivro02 = new ArrayList<>();
+		autoresLivro02.add("Harvey Deitel");
+		
+		livro02.setAutores(autoresLivro02);
+		livro02.setIsbn("0130341517");
+		livro02.setAno(1999);
+		System.out.println("\nLivro 02: " + livro02);
+		
+		
+		/**
+		 * Usando o contrutor para popular um objeto.
+		 */
+		ArrayList<String> autores = new ArrayList<>();
+		autores.add("Kathy Sierra");
+		autores.add("Bert Bates");
+		Livro livro03 = new Livro("9865PLO", "Use a cabeça! Java", autores, "0138426846", 2003);
+
+		System.out.println("\nLivro 03: " + livro03);
+
+	}
+}
